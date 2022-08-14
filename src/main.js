2bf60d336b2bb8456e8ea613f9d215e444ba9462
youngguyz <2281300996@qq.com>
2022-08-14 11:23:30 +0800

    :rocket: 引入ui组件库、设置axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入 ui组件
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 引入 axios 库
+import axios from 'axios'
+
+// 设置请求基地址
+axios.defaults.baseURL = 'http://www.liulongbin.top:3008'
+
+// 挂载到 vue 原型上去
+Vue.prototype.$http = axios
+
+// 注册使用 ui组件
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
