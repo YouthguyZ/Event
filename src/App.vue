e540a1f2a6bac1c63937374c93f18a75bc639e7e
youngguyz <2281300996@qq.com>
2022-08-14 11:09:02 +0800

    :rocket: update

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div>
+
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: 'Bigevent3App',
 
-nav {
-  padding: 30px;
+  data () {
+    return {
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
     }
+  },
+
+  mounted () {
+
+  },
+
+  methods: {
+
   }
 }
+</script>
+
+<style lang="scss" scoped>
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// 注册使用 vuex
 Vue.use(Vuex)
 
 export default new Vuex.Store({
